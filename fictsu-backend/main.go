f8794ba79803deb672ba7e1b6d9123e2fd465db3
kwangsensei <kwangsensei@outlook.com>
2025-01-29 01:55:55 +0700

    Add connection to database and routing URL

--- fictsu-backend/main.go
+++ fictsu-backend/main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 
-	// handlers "github.com/Fictsu/Fictsu/handlers"
+	db "github.com/Fictsu/Fictsu/database"
+	handlers "github.com/Fictsu/Fictsu/handlers"
 )
 
 func main() {
+	db.Connection()
 	router := gin.Default()
+
+	router.GET("/", handlers.GetAllFictions)
+	router.GET("/:fiction_id", handlers.GetFiction)
+	router.GET("/:fiction_id/:chapter_id", handlers.GetChapter)
 
 	router.Run()
 }
