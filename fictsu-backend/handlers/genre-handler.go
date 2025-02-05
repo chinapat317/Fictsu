59e229d3357ce76c3aa23ddbae0f2b349dd47836
kwangsensei <kwangsensei@outlook.com>
2025-02-05 10:45:07 +0700

    Separete from fiction handler

---
+++ fictsu-backend/handlers/genre-handler.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"fmt"
+
+	db "github.com/Fictsu/Fictsu/database"
+	models "github.com/Fictsu/Fictsu/models"
+)
+
+func GetAllGenres(ficion_id int) ([]models.GenreModel, error) {
+	rows, err := db.DB.Query(
+		"SELECT ID, Genre_Name FROM Genres JOIN AssignGenretoFiction ON ID = Genre_ID WHERE Fiction_ID = $1 ORDER BY ID", ficion_id,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to retrieve genres")
+	}
+
+	defer rows.Close()
+	genres := []models.GenreModel{}
+	for rows.Next() {
+		genre := models.GenreModel{}
+		if err := rows.Scan(
+			&genre.ID,
+			&genre.Genre_Name,
+		); err != nil {
+			return nil, fmt.Errorf("failed to process genre data")
+		}
+
+		genres = append(genres, genre)
+	}
+
+	return genres, nil
+}
