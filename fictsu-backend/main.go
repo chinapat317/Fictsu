673f892547e7ba5b5f55c88707384c9399788e37
kwangsensei <kwangsensei@outlook.com>
2025-02-05 10:43:24 +0700

    Update backend API

--- fictsu-backend/main.go
+++ fictsu-backend/main.go
@@ -9,11 +9,24 @@ import (
 
 func main() {
 	db.Connection()
+	defer db.CloseConnection()
+
 	router := gin.Default()
 
-	router.GET("/", handlers.GetAllFictions)
-	router.GET("/:fiction_id", handlers.GetFiction)
-	router.GET("/:fiction_id/:chapter_id", handlers.GetChapter)
+	api := router.Group("/api")
+
+	api.GET("/", handlers.GetAllFictions)
+	api.GET("/:fiction_id", handlers.GetFiction)
+	api.GET("/:fiction_id/:chapter_id", handlers.GetChapter)
+
+	api.POST("/", handlers.CreateFiction)
+	api.POST("/:fiction_id", handlers.CreateChapter)
+
+	api.PUT("/:fiction_id", handlers.EditFiction)
+	api.PUT("/:fiction_id/:chapter_id", handlers.EditChapter)
+
+	api.DELETE("/:fiction_id", handlers.DeleteFiction)
+	api.DELETE("/:fiction_id/:chapter_id", handlers.DeleteChapter)
 
 	router.Run()
 }
