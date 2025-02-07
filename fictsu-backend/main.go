c5e82e88ab441738b2cb302d8275d5c9d81ca5e5
kwangsensei <kwangsensei@outlook.com>
2025-02-07 13:43:09 +0700

    Add path for OAuth and session

--- fictsu-backend/main.go
+++ fictsu-backend/main.go
@@ -1,13 +1,44 @@
 package main
 
 import (
+	"os"
+	"log"
+	"github.com/joho/godotenv"
 	"github.com/gin-gonic/gin"
+	"github.com/markbates/goth"
+	"github.com/gorilla/sessions"
+	"github.com/markbates/goth/providers/google"
 
 	db "github.com/Fictsu/Fictsu/database"
 	handlers "github.com/Fictsu/Fictsu/handlers"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	client_id := os.Getenv("CLIENT_ID")
+	client_secret := os.Getenv("CLIENT_SECRET")
+	client_callback_URL := os.Getenv("CLIENT_CALLBACK_URL")
+
+	goth.UseProviders(
+		google.New(
+			client_id,
+			client_secret,
+			client_callback_URL,
+			"email", "profile",
+		),
+	)
+	
+	store := sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
+	store.Options = &sessions.Options{
+		HttpOnly: true, 	// Prevent JavaScript access to the cookie
+    	Secure:   false, 	// Set to true in production (requires HTTPS)
+		MaxAge:   1209600, 	// 2 weeks in seconds
+	}
+
 	db.Connection()
 	defer db.CloseConnection()
 
@@ -18,6 +49,10 @@ func main() {
 	api.GET("/", handlers.GetAllFictions)
 	api.GET("/:fiction_id", handlers.GetFiction)
 	api.GET("/:fiction_id/:chapter_id", handlers.GetChapter)
+	api.GET("/auth/:provider", handlers.Authen)
+    api.GET("/auth/:provider/callback", func(ctx *gin.Context) {
+		handlers.Callback(ctx, store)
+	})
 
 	api.POST("/", handlers.CreateFiction)
 	api.POST("/:fiction_id", handlers.CreateChapter)
