44235e1e0806afaf27d679647abc3b7d58a283af
kwangsensei <kwangsensei@outlook.com>
2025-02-08 23:55:10 +0700

    Add session management

--- fictsu-backend/handlers/oauth-handler.go
+++ fictsu-backend/handlers/oauth-handler.go
@@ -5,9 +5,11 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/sessions"
 	"github.com/markbates/goth/gothic"
+
+	models "github.com/Fictsu/Fictsu/models"
 )
 
-func Authen(ctx *gin.Context) {
+func GetOpenAuthorization(ctx *gin.Context) {
 	provider := ctx.Param("provider")
 	query := ctx.Request.URL.Query()
 	query.Add("provider", provider)
@@ -22,32 +24,59 @@ func Callback(ctx *gin.Context, store *sessions.CookieStore) {
 	query.Add("provider", provider)
 	ctx.Request.URL.RawQuery = query.Encode()
 
-	user, err := gothic.CompleteUserAuth(ctx.Writer, ctx.Request)
-	if err != nil {
-		ctx.IndentedJSON(http.StatusInternalServerError, gin.H{"Error": err.Error()})
+	user, err_complete := gothic.CompleteUserAuth(ctx.Writer, ctx.Request)
+	if err_complete != nil {
+		ctx.IndentedJSON(http.StatusInternalServerError, gin.H{"Error1": err_complete.Error()})
 		return
 	}
 
-	session, err := store.Get(ctx.Request, "fictsu-session")
-	if err != nil {
-		ctx.IndentedJSON(http.StatusInternalServerError, gin.H{"Error": "Failed to create session"})
+	// Check if the user exists in the database
+	user_in_db, err_get := GetUser(user.UserID)
+	if err_get != nil {
+		ctx.IndentedJSON(http.StatusInternalServerError, gin.H{"Error2": err_get.Error()})
 		return
 	}
 
-	session.Values["userID"] = user.UserID
-	session.Values["name"] = user.Name
-	session.Values["email"] = user.Email
-	session.Values["avatar"] = user.AvatarURL
-	err = session.Save(ctx.Request, ctx.Writer)
-	if err != nil {
-		ctx.IndentedJSON(http.StatusInternalServerError, gin.H{"Error": "Failed to save session"})
+	// Create a new user if not found
+	if user_in_db == nil {
+		new_user := &models.UserModel{
+			User_ID: user.UserID,
+			Name: user.Name,
+			Email: user.Email,
+			Avatar_URL: user.AvatarURL,
+		}
+
+		created_user, err_create_user := CreateUser(new_user)
+		if err_create_user != nil {
+			ctx.IndentedJSON(http.StatusInternalServerError, gin.H{"Error3": err_create_user.Error()})
+			return
+		}
+
+		user_in_db = created_user
+	}
+
+	session, err_sess := store.Get(ctx.Request, "fictsu-session")
+	if err_sess != nil {
+		ctx.IndentedJSON(http.StatusInternalServerError, gin.H{"Error4": "Failed to create session"})
+		return
+	}
+
+	session.Values["userID"] = user_in_db.User_ID
+	session.Values["name"] = user_in_db.Name
+	session.Values["email"] = user_in_db.Email
+	session.Values["avatar"] = user_in_db.Avatar_URL
+	err_sess = session.Save(ctx.Request, ctx.Writer)
+	if err_sess != nil {
+		ctx.IndentedJSON(http.StatusInternalServerError, gin.H{"Error5": "Failed to save session"})
 		return
 	}
 
 	ctx.IndentedJSON(http.StatusOK, gin.H{
-		"ID": user.UserID,
-		"Name": user.Name,
-		"Email": user.Email,
-		"Avatar": user.AvatarURL,
+		"ID": user_in_db.ID,
+		"User_ID": user_in_db.User_ID,
+		"Name": user_in_db.Name,
+		"Email": user_in_db.Email,
+		"Avatar_URL": user_in_db.Avatar_URL,
+		"Joined": user_in_db.Joined,
 	})
 }
