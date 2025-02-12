59c7981d9ff5fe4c7dd8708d4b1ce5c01c92cf27
kwangsensei <kwangsensei@outlook.com>
2025-02-12 18:54:30 +0700

    Add favorite and contributed fictions to UserModel

--- fictsu-backend/models/user-model.go
+++ fictsu-backend/models/user-model.go
@@ -5,11 +5,13 @@ import (
 )
 
 type UserModel struct {
-	ID        	int			`json:"id"`
-	User_ID    	string		`json:"user_id"`
-	Super_User 	int			`json:"super_user"`
-	Name      	string		`json:"name"`
-	Email     	string		`json:"email"`
-	Avatar_URL 	string		`json:"avatar_url"`
-	Joined 		time.Time	`json:"joined"`
+	ID        			int				`json:"id"`
+	User_ID    			string			`json:"user_id"`
+	Super_User 			int				`json:"super_user"`
+	Name      			string			`json:"name"`
+	Email     			string			`json:"email"`
+	Avatar_URL 			string			`json:"avatar_url"`
+	Joined 				time.Time		`json:"joined"`
+	Fav_Fictions 		[]FictionModel	`json:"fav_fictions"`
+	Contributed_Fic		[]FictionModel	`json:"contributed_fic"`
 }
